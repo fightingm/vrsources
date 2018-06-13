b188bbff2e1893085558c5c036840bf29e01f8c8
kaiming.xu <kaiming.xu@qunar.com>
2018-06-13 15:47:19 +0800

    add

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 > 为vr-panorama生成全景图, 基于gm
 
-### 使用方法
+### 准备工作
 
 安装 graphicsmagick
 
@@ -22,10 +22,22 @@ git clone https://github.com/fightingm/vrsources.git
 yarn / npm i
 ```
 
-将目录中test.jpg换成你的全景图，然后执行
+### 将全景图切割成碎片图
 
-```bash
-node index.js
+在目录下准备一张sphere.jpg,执行`npm run sphere`
+切割好的图片就会在sphere目录中
+
+### 将立方体的六个面拼接成全景图
+
+在目录下新建cube文件夹，放入六个面的图片，命名规则：
+假设我们现在在盒子内部中心点，面向屏幕外
 ```
-
-切割好的图片就会在dest目录中
+pano1: 背面图
+pano2: 右面图
+pano3: 前面图
+pano4: 左面图
+pano5: 下面图
+pano6: 上面图
+```
+然后执行`npm run cube`
+会生成一个拼接好的图片cube.jpg
--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const gm = require('gm');
 const fs = require('fs');
 
+const arg = process.argv.splice(2)[0];
+
+const SPHERE_INFO = {
+    src: './sphere.jpg',
+    dest: './sphere'
+};
+const CUBE_INFO = {
+    src: './cube.jpg',
+    dest: './cube'
+};
+
+
 function size(srcImg) {
     return new Promise((resolve, reject) => {
         gm(srcImg).size(function(err, value){
@@ -17,11 +29,11 @@ function crop(srcImg, destImg, width, height, x, y) {
     });
 }
 
-function mkdir() {
+function mkdir(dir) {
     return new Promise((resolve, reject) => {
-        fs.stat('dest', (err, stats) => {
+        fs.stat(dir, (err, stats) => {
             if(err) {
-                fs.mkdir('dest', err => {
+                fs.mkdir(dir, err => {
                     console.log(err);
                 });
             }
@@ -30,21 +42,37 @@ function mkdir() {
     })
 }
 
-
-
-async function sliceImg(srcImg) {
-    await mkdir();
-    const { width, height } = await size(srcImg);
+// 将球面全景图切割成碎片图
+async function sphereSlice(objInfo) {
+    const { src, dest } = objInfo;
+    await mkdir(dest);
+    const { width, height } = await size(src);
     const sliceWidth = width / 8;
     const sliceHeight = height / 4;
     for(let i = 0; i < 8; i++) {
         for(let j = 0; j < 4; j++) {
-            const destImg = `./dest/pano${i}-${j}.jpg`;
+            const destImg = `${dest}/pano${i}-${j}.jpg`;
             const x = i * sliceWidth;
             const y = j * sliceHeight;
-            crop(srcImg, destImg, sliceWidth, sliceHeight, x, y);
+            crop(src, destImg, sliceWidth, sliceHeight, x, y);
         }
     }
 }
 
-sliceImg('./test.jpg');
+// 将立方体的每一个面拼接成全景图
+async function cubeSlice(objInfo) {
+    const { src, dest } = objInfo;
+    let arr = [];
+    for(let i = 1; i < 7; i++) {
+        arr.push(`${dest}/pano${i}.jpg`)
+    }
+    gm(arr.splice(0, 1)).append(...arr, true).write(src, err =>{
+        console.log(err);
+    });
+}
+
+if(arg === 'cube') {
+    cubeSlice(CUBE_INFO)
+} else {
+    sphereSlice(SPHERE_INFO)
+}
